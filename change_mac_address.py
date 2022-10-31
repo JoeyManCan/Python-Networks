a15e105bfe0b3088d6bce47148d320576cdd27c2
JosephOak <joseph.oak.can@gmail.com>
2022-10-30 18:15:50 -0700

    - command line parameters implemented

--- change_mac_address.py
+++ change_mac_address.py
@@ -2,18 +2,23 @@ import optparse
 import subprocess
 
 parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest = "interface",
-                  help = "Interface which MAC address will be changed")
-parser.add_option("-m", "--mac", dest = "new_mac",
-                  help = "New MAC Address")
-parser.parse_args()
-
-def change_mac_address(interface, new_mac, shell = True):
-    subprocess.call(f"ifconfig {interface} down", shell = shell)
-    subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell = shell)
-    subprocess.call(f"ifconfig {interface} up", shell = shell)
+parser.add_option("-i", "--interface", dest="interface",
+                  help="Interface which MAC address will be changed")
+parser.add_option("-m", "--mac", dest="mac_address",
+                  help="New MAC Address")
+(values, arguments) = parser.parse_args()
+interface = values.interface
+mac_address = values.mac_address
 
 
-interface = input("enter interface: ")
-mac_address = input("enter mac address: ")
-change_mac_address(interface,mac_address)
+def change_mac_address(interface_value, new_mac, shell=True):
+    subprocess.call(f"ifconfig {interface_value} down", shell=shell)
+    subprocess.call(f"ifconfig {interface_value} hw ether {new_mac}", shell=shell)
+    subprocess.call(f"ifconfig {interface_value} up", shell=shell)
+
+    subprocess.call(f"ifconfig {interface_value}")
+
+
+# interface = input("enter interface: ")
+# mac_address = input("enter mac address: ")
+change_mac_address(interface, mac_address)
