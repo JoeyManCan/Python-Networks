f3570df2a01b2425ba5d41b6fba3156bda6eb39e
JosephOak <joseph.oak.can@gmail.com>
2022-10-30 18:33:49 -0700

    - showing terminal commands on prompt

--- change_mac_address.py
+++ change_mac_address.py
@@ -1,24 +1,27 @@
 import optparse
 import subprocess
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface",
-                  help="Interface which MAC address will be changed")
-parser.add_option("-m", "--mac", dest="mac_address",
-                  help="New MAC Address")
-(values, arguments) = parser.parse_args()
-interface = values.interface
-mac_address = values.mac_address
+
+def get_command_line_parameters():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="Interface which MAC address will be changed")
+    parser.add_option("-m", "--mac", dest="mac_address",
+                      help="New MAC Address")
+    return parser.parse_args()
+
+
 
 
 def change_mac_address(interface_value, new_mac, shell=True):
     subprocess.call(f"ifconfig {interface_value} down", shell=shell)
     subprocess.call(f"ifconfig {interface_value} hw ether {new_mac}", shell=shell)
     subprocess.call(f"ifconfig {interface_value} up", shell=shell)
+    subprocess.call(["ifconfig", f"{interface_value}"])
 
-    subprocess.call(f"ifconfig {interface_value}")
 
+(values, arguments) = get_command_line_parameters()
+interface = values.interface
+mac_address = values.mac_address
 
-# interface = input("enter interface: ")
-# mac_address = input("enter mac address: ")
 change_mac_address(interface, mac_address)
